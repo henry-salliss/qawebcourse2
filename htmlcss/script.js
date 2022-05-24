c8113e2c0390872d3478bf62abe2a52bd358d5a2
henry.salliss <henrysalliss@gmail.com>
2022-05-24 13:41:50 +0100

    completed last two labs

--- htmlcss/script.js
+++ htmlcss/script.js
@@ -64,20 +64,20 @@ console.log(div)
 
 // lab 4 and 5
 
-// const getNames = () => {
-//     const names = ['henry', 'tobi', 'molly', 'annabel', 'rosie']
-//     const ages = [21,21,21,24,27]
-//     let result = ''
-//     for(let i = 0; i < names.length; i++){
-//         result += `${names[i]} `;
+const getNames = () => {
+    const names = ['henry', 'tobi', 'molly', 'annabel', 'rosie']
+    const ages = [21,21,21,24,27]
+    let result = ''
+    for(let i = 0; i < names.length; i++){
+        result += `${names[i]} `;
         
-//         div.innerHTML = names;
-//     }
+        div.innerHTML = names;
+    }
 
-//     return result;
-// }
+    return result;
+}
 
-// const names = getNames();
+const names = getNames();
 
 
 
@@ -92,3 +92,32 @@ const bigNumbers = () => {
 }
 
 bigNumbers();
+
+
+const json = `{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}`
+const parsedJSON = JSON.parse(json)
+console.log(parsedJSON)
+console.log(parsedJSON.glossary.title)
+// console.log(json.glossary.title)
+// console.log(json.glossary.GlossDef.para)
