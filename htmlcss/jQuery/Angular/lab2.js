002415b04aa797114466134c8d750e021a9b120e
henry.salliss <henrysalliss@gmail.com>
2022-05-26 15:05:04 +0100

    completed lab on events in angular js

--- htmlcss/jQuery/Angular/lab2.js
+++ htmlcss/jQuery/Angular/lab2.js
@@ -6,6 +6,20 @@ const myController = ($scope) => {
         {name: 'brioche', unitPrice: 2, stockLevel: 100},
         {name: 'protein yoghurt', unitPrice: 1, stockLevel: 15}
     ]
+
+    $scope.currentProduct = null;
+
+    $scope.products.selectProduct = (product) => {
+        $scope.currentProduct = product;
+    }
+
+    $scope.btnClick = (product) => {
+        console.log(product.name)
+    }
+
+    $scope.getDetails = (e) => {
+        alert(`clicked at ${e.clientX} and ${e.clientY}`)
+    }
 }
 
 myModule.controller('myController', myController)
